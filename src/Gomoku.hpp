94f359a3fe6e85c1c3e7cde5a4ac4a8464377025
kyaubry <aubry.kylian973@gmail.com>
2026-03-10 12:02:21 +0100

    add move

--- src/Gomoku.hpp
+++ src/Gomoku.hpp
@@ -1,5 +1,6 @@
 #pragma once
 #include <array>
+#include <chrono>
 
 constexpr int SIZE = 19; //19 x 19 board
 
@@ -10,16 +11,22 @@ enum Cell {
 };
 
 using Board = std::array<std::array<Cell, SIZE>, SIZE>;
-/* ==================== include ==================== */
 
-# include <unistd.h>
-# include <stdio.h>
+struct Move {
+    int row;
+    int col;
 
-/* ==================== structure ==================== */
+    long computeTimeMs;
+};
 
 class Gomoku {
 public:
     Board board;
+
     Gomoku();
 
+    Move getBestMove(const Board& board, Cell player);
+
+private:
+    Move minimax(const Board& board, Cell player);
 };
